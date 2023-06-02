38beae02c233379f6691572302d82411fc830aec
Gustavo Delbon Souza <61765239+MrDelbon@users.noreply.github.com>
2023-06-02 19:28:09 -0300

    Atualização de API

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   useEffect(() => {
     const fetchTasks = async () => {
       const { data } = await axios.get(
-        "https://jsonplaceholder.cypress.io/todos?_limit=10"
+        "https://my-json-server.typicode.com/luizfpq/tads-api-server"
       );
       setTasks(data);
     };
